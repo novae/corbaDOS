35ffb57d903d0764a50a1b1c2c1365de703dad31
TEC <TEC@TEC-PC>
2012-04-14 07:59:52 -0500

    version 1.0 add serverClass

--- src/ServidorContador.java
+++ src/ServidorContador.java
@@ -33,3 +33,33 @@ class ContadorImpl extends ContadorPOA  {
        orb.shutdown(false);  
    }
 }
+
+
+
+public class ServidorContador{  
+    public static void main(String args[]){    
+     
+    try{ 
+         ORB orb = ORB.init(args, null);      
+         POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));      
+         rootpoa.the_POAManager().activate();      
+         ContadorImpl contadorImpl = new ContadorImpl();      
+         contadorImpl.setORB(orb);       
+         org.omg.CORBA.Object ref = rootpoa.servant_to_reference(contadorImpl);      
+         Contador href = ContadorHelper.narrow(ref); 
+         org.omg.CORBA.Object objRef =orb.resolve_initial_references("NameService");     
+         NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);      
+         String name = "Contador";      
+         NameComponent path[] = ncRef.to_name( name );      
+         ncRef.rebind(path, href);      
+         System.out.println("Servicio de contador preparado y en espera de peticiones ...");    
+         orb.run();    
+      }  
+     catch (Exception e) 
+      {        System.err.println("ERROR: " + e);        
+               e.printStackTrace(System.out);      
+      }          
+     System.out.println("Servidor de contador desconectadose ...");    
+ }
+}
+
